358e59c6ad6aa145c5c3df76791da5286f51238e
omar eduardo carvajal guerra <carvajalomar123@gmail.com>
2017-11-11 21:14:07 -0500

    ¨RaspiCar¨

---
+++ Arrancar.py
@@ -0,0 +1,13 @@
+
+import subprocess
+# Apagar
+#subprocess.call("shutdown -s")
+
+
+a=7 
+b=8
+y=(a+b)*90
+print (y)
+
+
+
---
+++ Arrancaraction.php
@@ -0,0 +1,12 @@
+<?php
+//$salida= array(); //recogerá los datos que nos muestre el script de Python
+ 
+  //  $texto="Hola Mundo";
+    //exec("python contar.py '".$texto."'",$salida);
+    //echo $salida[0];
+
+
+  echo "ya estoy andandsdasdado -----------------------------------------------------------------------gg------------------------------------------------------------------------------->"; // Your code here
+
+echo exec('python Arrancar.py');
+?> 
